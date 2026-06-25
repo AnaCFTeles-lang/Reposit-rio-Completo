ee4a01dce586b0bfb7a8ceec4db3b0f2c7751c63
AnaCFTeles-lang <afontourateles@gmail.com>
2026-06-25 15:00:44 -0300

    Criar inserir_teste.sql

---
+++ scr/database/inserir_teste.sql
@@ -0,0 +1,5 @@
+INSERT INTO usuarios (nome, email, senha, perfil, curso_turma)
+VALUES 
+('Administrador', 'admin@napne.br', '123456', 'admin', '2026‑1'),
+('Maria Alves', 'professor@napne.br', '123456', 'professor', '2026‑1'),
+('João da Silva', 'aluno@napne.br', '123456', 'aluno', '2026‑1');
